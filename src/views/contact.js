a7f42311a38db3d934c4dc90e2fcd551bf4bf044
Alberto Pereira <alberto.pereira.gyn@gmail.com>
2022-02-17 17:54:25 -0300

    contact
    - added option to send a form message to my email

--- src/views/contact.js
+++ src/views/contact.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import emailjs from '@emailjs/browser';
 
 class Contact extends React.Component{
     render(){
+        const sendEmail = (e) => {
+            e.preventDefault();
+
+            emailjs.sendForm('gmail', 'template_8dh8oek', e.target, 'user_DsrRZtscvm0Z9gDWH3kWe')
+                .then((result) => {
+                    console.log(result.text);
+                }, (error) => {
+                    console.log(error.text);
+                });
+                e.target.reset();
+        }
+
         return(
             <div className='container-fluid custom-bg-color'>
                 <div className='row'>
@@ -12,13 +25,13 @@ class Contact extends React.Component{
                                     <h4 className='custom-card-header-text'>Como posso ajudar?</h4>
                                 </div>
                                 <div className='card-body'>
-                                    <form>
-                                        <input type='text' className='form-control mb-1' placeholder='Nome'/>
-                                        <input type='email' className='form-control mb-1' placeholder='Email'/>
-                                        <input type='text' className='form-control mb-1' placeholder='Assunto'/>
-                                        <textarea type='text' className='form-control mb-2' placeholder='Mensagem' rows='4'/>
+                                    <form onSubmit={sendEmail}>
+                                        <input type='text' className='form-control mb-1' placeholder='Nome' name='name'/>
+                                        <input type='email' className='form-control mb-1' placeholder='Email' name='email'/>
+                                        <input type='text' className='form-control mb-1' placeholder='Assunto' name='subject'/>
+                                        <textarea type='text' className='form-control mb-2' placeholder='Mensagem' rows='4' name='message'/>
                                         <button className="btn custom-contact-section-btn float-end" 
-                                                style={{width:'30%'}}>Enviar</button>
+                                                style={{width:'30%'}} type="submit" value="Send">Enviar</button>
                                     </form>
                                 </div>
                             </div>
